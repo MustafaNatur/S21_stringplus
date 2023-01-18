6fa6c22f79b804039420f6fb15d926121c4b7fcc
Loralee Chong <loraleec@su-d6.msk.21-school.ru>
2023-01-18 15:26:20 +0300

    loraleec: change size_t -> s21_size_t

--- src/s21_string.h
+++ src/s21_string.h
@@ -8,23 +8,23 @@ typedef unsigned int s21_size_t;
 // void * https://pvs-studio.com/ru/blog/terms/0088/ про терминирующий 0
 // http://natalia.appmat.ru/c&c++/lezione5.php обо всем по чуть-чуть
 
-void *memchr(const void *str, int c, size_t n);
-int memcmp(const void *str1, const void *str2, size_t n);
-void *memcpy(void *dest, const void *src, size_t n);
-void *memmove(void *dest, const void *src, size_t n);
-void *memset(void *str, int c, size_t n);
+void *memchr(const void *str, int c, s21_size_t n);
+int memcmp(const void *str1, const void *str2, s21_size_t n);
+void *memcpy(void *dest, const void *src, s21_size_t n);
+void *memmove(void *dest, const void *src, s21_size_t n);
+void *memset(void *str, int c, s21_size_t n);
 char *strcat(char *dest, const char *src);
-char *strncat(char *dest, const char *src, size_t n);
+char *strncat(char *dest, const char *src, s21_size_t n);
 char *strchr(const char *str, int c);
 int strcmp(const char *str1, const char *str2);
-int strncmp(const char *str1, const char *str2, size_t n);
+int strncmp(const char *str1, const char *str2, s21_size_t n);
 char *strcpy(char *dest, const char *src);
-char *strncpy(char *dest, const char *src, size_t n);
-size_t strcspn(const char *str1, const char *str2);
+char *strncpy(char *dest, const char *src, s21_size_t n);
+s21_size_t strcspn(const char *str1, const char *str2);
 char *strerror(int errnum);
-size_t strlen(const char *str);
+s21_size_t strlen(const char *str);
 char *strpbrk(const char *str1, const char *str2);
 char *strrchr(const char *str, int c);
-size_t strspn(const char *str1, const char *str2);
+s21_size_t strspn(const char *str1, const char *str2);
 char *strstr(const char *haystack, const char *needle);
 char *strtok(char *str, const char *delim);
