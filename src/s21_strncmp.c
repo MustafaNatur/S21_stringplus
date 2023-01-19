3b4072f1d167025bc492fde664ddea88e8b70501
ZeevBinyaminov <zzbinyaminov@outlook.com>
2023-01-19 17:37:24 +0300

    strncmp

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -1,31 +1,13 @@
 #include "s21_string.h"
 
-int strncmp(const char *str1, const char *str2, s21_size_t n) {
-  int ret = 1, i = 0;
-  while (str1[i] != '\0' && str2[i] != '\0') {
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
+  int ret = 0, i = 0, counter = 0;
+  while ((str1[i] != '\0' || str2[i] != '\0') && counter < n) {
     if (str1[i] != str2[i]) {
-      ret = 0;
+      ret = str1[i] - str2[i];
       break;
     }
-    i++;
-  }
-  if (str1[i] != '\0' || str2[i] != '\0') {
-    ret = 0;
-  }
-  return ret;
-}
-
-int s21_strcmp(const char *left, const char *right) {
-  int ret = 1, i = 0;
-  while (left[i] != '\0' && right[i] != '\0') {
-    if (left[i] != right[i]) {
-      ret = 0;
-      break;
-    }
-    i++;
-  }
-  if (left[i] != '\0' || right[i] != '\0') {
-    ret = 0;
+    i++, counter++;
   }
   return ret;
 }
