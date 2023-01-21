95cc6d39b29f30ec0e641704ed7b613bcd4baee4
Loralee Chong <loraleec@mi-u6.msk.21-school.ru>
2023-01-21 16:07:34 +0300

    loraleec: strchr and test

--- src/sourceFiles/s21_strcmp.c
+++ src/sourceFiles/s21_strcmp.c
@@ -1,7 +1,6 @@
 #include "../s21_string.h"
 
 int s21_strcmp(const char *str1, const char *str2) {
-  // int cmp_res = 0;
   while (*str1 != '\0' && *str2 != '\0' && *str1 == *str2) {
     str1++;
     str2++;
